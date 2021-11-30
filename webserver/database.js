62d9237bed62b95b239c5d4be253c65096889a87
Tanying <tlin74832@gmail.com>
2021-11-30 11:40:17 -0500

    added the resultinfo table

--- webserver/database.js
+++ webserver/database.js
@@ -15,6 +15,9 @@ if (row === undefined) {
 // Set a const that will contain your SQL commands to initialize the database.
     const sqlInit = `
         CREATE TABLE userinfo ( id INTEGER PRIMARY KEY, user TEXT UNIQUE, pass TEXT, salt TEXT);
+		INSERT INTO userinfo (user, pass) VALUES ('admin','bdc87b9c894da5168059e00ebffb9077');
+        CREATE TABLE resultinfo (resultId INTEGER PRIMARY KEY, result TEXT, userId INTEGER references userinfo(id));
+        INSERT INTO resultinfo (userId, result) VALUES (1, 'Gryffindor Lion');
     `;
 // Execute SQL commands that we just wrote above.
     db.exec(sqlInit);
