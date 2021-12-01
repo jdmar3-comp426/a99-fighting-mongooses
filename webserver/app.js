206ff5f8c7e890e1f1411876b4074b79393ac36e
Cody <irion@live.unc.edu>
2021-12-01 07:54:10 -0500

    added additional cors configuration

--- webserver/app.js
+++ webserver/app.js
@@ -21,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.options('*', cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
