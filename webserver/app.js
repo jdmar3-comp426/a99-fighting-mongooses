fade79932f22bfe16ea4d2159ef6a1becec3844e
Cody <irion@live.unc.edu>
2021-12-01 18:28:28 -0500

    set specific host

--- webserver/app.js
+++ webserver/app.js
@@ -21,10 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.options('*', cors());
-app.use(cors());
+//app.options('*', cors());
+//app.use(cors());
+
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
